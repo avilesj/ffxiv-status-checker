c85348acb97529a395df930b22a0768e7cf4ca61
Jose Aviles <javiles@pop-os.localdomain>
2021-08-29 15:19:47 -0400

    Split in go routines and store value. Refactor

--- main.go
+++ main.go
@@ -4,11 +4,37 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
+type Realm struct {
+	name     string
+	category string
+	status   string
+}
+
+func getServerData(s *goquery.Selection, wg *sync.WaitGroup, r []Realm, i int) {
+	defer wg.Done()
+	name, err := s.Find(".world-list__world_name > p").First().Html()
+	if err != nil {
+		log.Fatal(err)
+	}
+	category, err := s.Find(".world-list__world_category > p").First().Html()
+	if err != nil {
+		log.Fatal(err)
+	}
+	status, _ := s.Find(".world-list__create_character > i").First().Attr("data-tooltip")
+	r[i] = Realm{
+		name:     name,
+		category: category,
+		status:   status,
+	}
+}
+
 func main() {
+	var wg sync.WaitGroup
 	res, err := http.Get("https://na.finalfantasyxiv.com/lodestone/worldstatus/")
 	if err != nil {
 		fmt.Print(err)
@@ -19,18 +45,11 @@ func main() {
 		log.Fatal(err)
 	}
 
-	doc.Find(".world-list__item").Each(func(i int, s *goquery.Selection) {
-		name, err := s.Find(".world-list__world_name > p").First().Html()
-		if err != nil {
-			log.Fatal(err)
-		}
-		category, err := s.Find(".world-list__world_category > p").First().Html()
-		if err != nil {
-			log.Fatal(err)
-		}
-		status, _ := s.Find(".world-list__create_character > i").First().Attr("data-tooltip")
-		fmt.Printf("Realm: %s\n", name)
-		fmt.Printf("Status: %s\n", status)
-		fmt.Printf("Category: %s\n", category)
+	servers := doc.Find(".world-list__item")
+	r := make([]Realm, servers.Length())
+	servers.Each(func(i int, s *goquery.Selection) {
+		wg.Add(1)
+		go getServerData(s, &wg, r, i)
 	})
+	wg.Wait()
 }
